dda22bade7922e8e4dc5327f23b29f8371e5ddf1
wagneced <cedricwagner95@gmail.com>
2022-10-09 17:42:27 +0200

    Delete template project settings
    Cleaning project of entries of template project

--- src/main/java/ch/zhaw/springboot/InfMApplication.java
+++ src/main/java/ch/zhaw/springboot/InfMApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Hauptklasse für die InfM-Applikation
+ * Hauptklasse für die Applikation
  * 
  * Die Applikation ist stark angelehnt an das Getting Started-Beispiel hier:
  * https://spring.io/guides/gs/rest-service/
